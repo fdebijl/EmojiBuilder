f11c9fe8beb87e33fce51e96b8f735609d30cbad
Fdebijl <floris.debijl@live.nl>
2017-10-11 19:41:01 +0200

    Dragging fully supported on mobile devices

--- src/js/emojibuilder.js
+++ src/js/emojibuilder.js
@@ -56,42 +56,46 @@ function appendSource(e) {
 
 // Enable dragging on this element
 function Draggable(elem) {
-    this.target = elem
-    this.clickPoint = this.target.ownerSVGElement.createSVGPoint()
-    this.lastMove = this.target.ownerSVGElement.createSVGPoint()
-    this.currentMove = this.target.ownerSVGElement.createSVGPoint()
-    this.target.addEventListener("mousedown", this)
-    this.handleEvent = function(evt) {
-        evt.preventDefault()
-        this.clickPoint = globalToLocalCoords(evt.clientX, evt.clientY)
-        this.target.classList.add("dragged")
-        this.target.setAttribute("pointer-events", "none")
-        this.target.ownerSVGElement.addEventListener("mousemove", this.move)
-        this.target.ownerSVGElement.addEventListener("mouseup", this.endMove)
-    }
-    this.move = function(evt) {
-        var p = globalToLocalCoords(evt.clientX, evt.clientY)
-        this.currentMove.x = this.lastMove.x + (p.x - this.clickPoint.x)
-        this.currentMove.y = this.lastMove.y + (p.y - this.clickPoint.y)
-        this.target.setAttribute("transform", "translate(" + this.currentMove.x + "," + this.currentMove.y + ")")
-    }.bind(this)
-
-    this.endMove = function(evt) {
-        this.lastMove.x = this.currentMove.x
-        this.lastMove.y = this.currentMove.y
-        this.target.classList.remove("dragged")
-        this.target.setAttribute("pointer-events", "all")
-        this.target.ownerSVGElement.removeEventListener("mousemove", this.move)
-        this.target.ownerSVGElement.removeEventListener("mouseup", this.endMove)
-    }.bind(this)
-
+	// Rewritten from https://stackoverflow.com/questions/41514967/yes-no-is-there-a-way-to-improve-mouse-dragging-with-pure-svg-tools/41518545
+    let target = elem;
+	
+	// Create SVG points for capturing...
+	// clickpoint: ... where the cursor is relative to the rest of the path
+	// lastMove: ... the last transformation applied to this element
+	// currentMove .. the current transformation to apply next
+	let lastMove    = target.ownerSVGElement.createSVGPoint();
+    let clickPoint  = target.ownerSVGElement.createSVGPoint();
+    let currentMove = target.ownerSVGElement.createSVGPoint();
+	
+	$(target).on("vmousedown", function(evt) {
+        evt.preventDefault();
+        clickPoint = globalToLocalCoords(evt.clientX, evt.clientY);
+        target.classList.add("dragged");
+        target.setAttribute("pointer-events", "none");
+        $(target.ownerSVGElement).on("vmousemove", target, function(evt) {
+			let p = globalToLocalCoords(evt.clientX, evt.clientY);
+			currentMove.x = lastMove.x + (p.x - clickPoint.x);
+			currentMove.y = lastMove.y + (p.y - clickPoint.y);
+			target.setAttribute("transform", "translate(" + currentMove.x + "," + currentMove.y + ")");
+		});
+		
+        $(target.ownerSVGElement).on("vmouseup", target, function(evt) {
+			lastMove.x = currentMove.x;
+			lastMove.y = currentMove.y;
+			target.classList.remove("dragged");
+			target.setAttribute("pointer-events", "all");
+			$(target.ownerSVGElement).off("vmousemove");
+			$(target.ownerSVGElement).off("vmouseup");
+		});
+    });
+	
     // Convert DOM coordinates to SVG coordinates so we can apply a translation to this path accordingly
     function globalToLocalCoords(x, y) {
-        var p = elem.ownerSVGElement.createSVGPoint()
-        var m = elem.parentNode.getScreenCTM()
-        p.x = x
-        p.y = y
-        return p.matrixTransform(m.inverse())
+        let p = target.ownerSVGElement.createSVGPoint();
+        let m = target.parentNode.getScreenCTM();
+        p.x = x;
+        p.y = y;
+        return p.matrixTransform(m.inverse());
 	}
 }
 
